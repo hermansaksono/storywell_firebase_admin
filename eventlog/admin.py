9249e1bd86195e46b1778502df2a00f03686fb04
Herman Saksono <herman.saksono@gmail.com>
2020-01-31 12:15:41 -0500

    Allow event admin to search for logs by username and event

--- eventlog/admin.py
+++ eventlog/admin.py
@@ -7,6 +7,7 @@ class LogAdmin(admin.ModelAdmin):
     list_display = ('user', 'date', 'event', 'count')
     list_display_links = ('user', 'date', 'event', 'count')
     ordering = ('date', 'event', 'user')
+    search_fields = ['user__user_id', 'event']
 
 
 admin.site.register(Log, LogAdmin)
