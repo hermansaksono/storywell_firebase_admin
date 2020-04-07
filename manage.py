ebf1979a5cf2c58ec736010b2b1ba793522e47ea
Herman Saksono <herman.saksono@gmail.com>
2020-04-06 21:21:37 -0400

    Rename project name

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'user_logging_processor.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'storywell_app_admin.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
