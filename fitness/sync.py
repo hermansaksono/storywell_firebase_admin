f46e394576eb7c9b99c8c4be2d39bb7e461fc48e
Herman Saksono <herman.saksono@gmail.com>
2020-04-07 11:08:54 -0400

    Change request sync payload from personId to userId

--- fitness/sync.py
+++ fitness/sync.py
@@ -3,7 +3,7 @@ import requests
 from fitness import config
 
 def request(user_id) -> bool:
-    payload = {"data": {"personId": user_id}}
+    payload = {"data": {"userId": user_id}}
     post_request = requests.post(config.URL_SYNC_FITNESS, json=payload)
 
     if post_request.status_code is 200:
